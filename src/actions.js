f24b04fb69dafc18e3065f8bbe52b778842fc5f8
tvanrijn <vanrijn.tineke@gmail.com>
2019-06-30 20:01:00 +0200

    fix merge conflict actions.js

--- src/actions.js
+++ src/actions.js
@@ -58,15 +58,8 @@ export const performSearch = () => (dispatch, getState) => {
         .catch(error => {
             dispatch({type: SEARCH_FAIL, payload: error})
         });
-<<<<<<< HEAD
     }, 1500);
 }
-=======
-};
-
-
-
->>>>>>> af0f2c86c2761e19f7d95858af6484efee65d09b
 
 export const changeRoute = (route) => ({
     type: CHANGE_ROUTE,
