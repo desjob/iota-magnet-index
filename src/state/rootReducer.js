d32a29df54bd722418d3d94f9ec8dddeb63cc2b4
Job Schipper <job.schipper@medicore.nl>
2019-07-04 17:42:53 +0200

    removed unused import

--- src/state/rootReducer.js
+++ src/state/rootReducer.js
@@ -3,6 +3,6 @@ import {combineReducers} from 'redux';
 import {navigation} from "../containers/navigation/reducers";
 import {searchCriteria, searchResults} from "../containers/searchPage/reducers";
 import {publish} from '../containers/publishPage/reducers';
-import {subscriptions, createSearchIndex} from '../containers/subscriptionsPage/reducers';
+import {subscriptions} from '../containers/subscriptionsPage/reducers';
 
 export const rootReducer = combineReducers({searchCriteria, searchResults, subscriptions, navigation, publish});
