5b0fd223ad151f812403d08f85586fc2f6c8bfa1
tvanrijn <vanrijn.tineke@gmail.com>
2019-08-28 15:28:33 +0200

    add unit tests for torrentsearch

--- src/components/torrentSearch/tests/torrentSearch.test.js
+++ src/components/torrentSearch/tests/torrentSearch.test.js
@@ -5,7 +5,7 @@ import TorrentSearch from '../torrentSearch';
 
 describe("Validate TorrentSearch", () => {
     const searchProps = {
-        searchQuery: '',
+        searchQuery: 'test',
         limit: 100,
         dateFrom: null,
         dateUntil: null,
@@ -15,7 +15,10 @@ describe("Validate TorrentSearch", () => {
         dialogDateUntil: null,
     };
 
-    const comp = mount(<TorrentSearch {...searchProps} />);
+    let comp;
+    beforeEach(() => {
+        comp = mount(<TorrentSearch {...searchProps} />);
+    });
 
     it("Should render the searchfield, limit filter and date filter", () => {
         expect(comp.find("input").length).toEqual(3);
@@ -23,5 +26,19 @@ describe("Validate TorrentSearch", () => {
 
     it("Should render the search and filter reset buttons", () => {
         expect(comp.find("button").length).toEqual(2);
+    });
+
+    it("Should show the searchquery in the searchfield", () => {
+        expect(
+            comp.find("input[type='search'][value='test']")
+                .length
+        ).toEqual(1);
+    });
+
+    it("Should show the limit in the limit filter", () => {
+        expect(
+            comp.find("input[id='limit'][value=100]")
+                .length
+        ).toEqual(1);
     });
 });
