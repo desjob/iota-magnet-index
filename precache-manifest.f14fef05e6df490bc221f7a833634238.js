0874fe1a79c55b285dd14b4383c762978b28c8bf
tvanrijn <vanrijn.tineke@gmail.com>
2019-12-02 22:56:48 +0100

    Updates

---
+++ precache-manifest.f14fef05e6df490bc221f7a833634238.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bfa2e59a8d10f3cef962952f9750861",
+    "url": "/iota-magnet-index/index.html"
+  },
+  {
+    "revision": "c8f4418aabba3b6e197b",
+    "url": "/iota-magnet-index/static/css/main.8e779c4a.chunk.css"
+  },
+  {
+    "revision": "b1752f0dfafb1f8bc6f5",
+    "url": "/iota-magnet-index/static/js/2.6efe6f28.chunk.js"
+  },
+  {
+    "revision": "c8f4418aabba3b6e197b",
+    "url": "/iota-magnet-index/static/js/main.a361f8db.chunk.js"
+  },
+  {
+    "revision": "807492a81e0b959b2496",
+    "url": "/iota-magnet-index/static/js/runtime~main.e1442af5.js"
+  }
+]);
